3c38b8048bc4129c5bd0ba80bca2000ccae8e590
paoloconza <paolocoha@gmail.com>
2023-10-22 14:24:16 -0500

    segundo commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,15 +8,33 @@ function SideBar({ dataPok, setActive }) {
             </div>
             <h2>{dataPok?.name}</h2>
             <div id="sideBar-divImg">
-                <img src={dataPok?.sprites.front_shiny} alt="Pokemon" />
-                <img src={dataPok?.sprites.front_default} alt="Pokemon" />
-                <img src={dataPok?.sprites.back_shiny} alt="Pokemon" />
-                <img src={dataPok?.sprites.back_default} alt="Pokemon" />
+                <img src={dataPok?.sprites.other.home.front_shiny} alt="Pokemon" />
             </div>
+
             <div id="sideBar-divText">
-                <p>PESO: {dataPok?.weight}</p>
-                <p>ALTURA: {dataPok?.height}</p>
-            </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Propiedad</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Altura</td>
+              <td>{dataPok?.height}</td>
+            </tr>
+            <tr>
+              <td>Peso</td>
+              <td>{dataPok?.weight}</td>
+            </tr>
+            <tr>
+              <td>Experiencia base</td>
+              <td>{dataPok?.base_experience}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
         </aside>
     )
 }
